c398a2b8c4dcf42a79c8236bbc098fe7eb194d1a
Ed Murphy <edzillion@gmail.com>
2019-05-16 11:59:53 +0200

    merge cafe-grundeinkommen from master manually

--- cafe-grundeinkommen-website/variables.tf
+++ cafe-grundeinkommen-website/variables.tf
@@ -1,12 +1,3 @@
-locals {
-  common_tags = {
-    team              = "${var.team}"
-    project           = "${var.project}"
-    environment       = "${var.environment}"
-    emergency_contact = "${var.emergency_contact}"
-  }
-}
-
 variable "access_key" {
   description = "AWS access key"
 }
@@ -17,32 +8,12 @@ variable "secret_key" {
 
 variable "aws_region" {
   description = "The AWS region to create things in."
-  default     = "eu-central-1"
 }
 
-variable "project" {
-  description = "Project of resources."
-  default     = "circles"
+variable "blueprint_id" {
+  description = "Blueprint for lightsail instance."  
 }
 
-variable "environment" {
-  description = "Environment setting."
-  default     = "dev"
-}
-
-variable "emergency_contact" {
-  description = "Who to contact in an emergency."
-}
-
-variable "aws_account_id" {
-  description = "The AWS account ID."
-}
-
-
-variable "availability_zones" {
-  default = ["eu-central-1a", "eu-central-1b"]
-}
-
-variable "circles_api_github_oauth_token" {
-  description = "OAuth Token for the circles-api github repo. https://help.github.com/articles/creating-a-personal-access-token-for-the-command-line/"
+variable "instance_size" {
+  description = "Size of lightsail instance"  
 }

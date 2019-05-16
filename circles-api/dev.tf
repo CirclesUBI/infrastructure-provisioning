bb2139f3382a91bfab3bfb45e090fc0e4be49837
Ed Murphy <edzillion@gmail.com>
2019-05-16 11:02:07 +0200

    mandate account id

--- circles-api/dev.tf
+++ circles-api/dev.tf
@@ -9,9 +9,9 @@ terraform {
 }
 
 provider "aws" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
-  region     = "${var.aws_region}"
+  access_key          = "${var.access_key}"
+  secret_key          = "${var.secret_key}"
+  region              = "${var.aws_region}"
   allowed_account_ids = ["${var.aws_account_id}"]
 }
 

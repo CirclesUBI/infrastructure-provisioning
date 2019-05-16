c0be518ccae230fcfa0e0b49234ee37734ebeb23
David Terry <me@xwvvvvwx.com>
2019-05-16 15:01:52 +0200

    deploy sns

--- circles-sns/modules/sms/main.tf
+++ circles-sns/modules/sms/main.tf
@@ -2,7 +2,7 @@ provider "aws" {} //initialise alias provider
 
 resource "aws_sns_sms_preferences" "update_sms_prefs" {
   usage_report_s3_bucket                = "${var.sms_log_bucket}"
-  monthly_spend_limit                   = "1000"
+  monthly_spend_limit                   = "200"
   delivery_status_iam_role_arn          = "${var.delivery_status_iam_role_arn}"
   delivery_status_success_sampling_rate = "100"
   default_sender_id                     = "CirclesUBI"
